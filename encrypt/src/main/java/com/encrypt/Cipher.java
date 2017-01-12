79a9b129100cc42141c0e1b64e713ba6a1762601
Oron <Oron@ORON>
2017-01-12 19:50:25 +0200

    Javadoc

--- encrypt/src/main/java/com/encrypt/Cipher.java
+++ encrypt/src/main/java/com/encrypt/Cipher.java
@@ -2,7 +2,11 @@ package com.encrypt;
 
 
 import lombok.Data;
-
+/***
+ * Abstract class which each cipher algorithm will extend it
+ * @author Oron
+ *
+ */
 @Data
 public abstract class Cipher {
 	
